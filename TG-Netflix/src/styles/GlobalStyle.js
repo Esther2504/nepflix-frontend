2337b773b4c1cbae0b3e0695d63033e366dca6f9
Bart Erkamp <b.erkamp@gmail.com>
2022-09-26 14:42:28 +0200

    Add css reset to global styles

--- TG-Netflix/src/styles/GlobalStyle.js
+++ TG-Netflix/src/styles/GlobalStyle.js
@@ -1,7 +1,64 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-body {
-  font-family: 'Manrope', sans-serif;
+
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
 }
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+html,
+body {
+  min-height: 100vh;
+}
+
+body {
+  line-height: 1.5;
+  font-family: 'Manrope', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  font-family: "Manrope", sans-serif;
+}
+
+img,
+picture,
+video,
+canvas,
+svg {
+  display: block;
+  max-width: 100%;
+}
+
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  overflow-wrap: break-word;
+}
+
+#root,
+#__next {
+  isolation: isolate;
+}
+
 `;
