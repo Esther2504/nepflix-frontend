052e532ba92a5b2f05eab05ec08460a86a97120e
Bart Erkamp <b.erkamp@gmail.com>
2022-09-26 14:35:36 +0200

    Add folder structure and remove unwanted files

--- TG-Netflix/src/main.jsx
+++ TG-Netflix/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
 import App from "./App";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyle />
     <BrowserRouter>
       <App />
     </BrowserRouter>
