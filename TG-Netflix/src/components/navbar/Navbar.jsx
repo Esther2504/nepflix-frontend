1949e253060dfcb45914734ad2bfba9f43c3e4d5
Lex <alext@disroot.org>
2022-09-27 17:36:55 +0200

    Add styled file

--- TG-Netflix/src/components/navbar/Navbar.jsx
+++ TG-Netflix/src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+
+import * as Styled from './Navbar.styled'
 import logo from '../../assets/netflix_logo.png';
 import profile from '../../assets/profile-icon.png';
 import { AiFillCaretDown } from 'react-icons/ai';
@@ -20,9 +21,9 @@ export default function NavBar() {
 
   return (
     <>
-      <Nav role="navigation">
-        <Container>
-          <PrimaryNav>
+      <Styled.Nav role="navigation">
+        <Styled.Container>
+          <Styled.PrimaryNav>
             <img src={logo} alt="Netflix logo" />
 
             <ul>
@@ -33,8 +34,8 @@ export default function NavBar() {
                 <a href="#">Films</a>
               </li>
             </ul>
-          </PrimaryNav>
-          <SecondaryNav>
+          </Styled.PrimaryNav>
+          <Styled.SecondaryNav>
             <ul>
               <li>
                 <TbSearch size={30} />
@@ -44,7 +45,7 @@ export default function NavBar() {
               </li>
               <li>
                 {' '}
-                <Profile>
+                <Styled.Profile>
                   <img src={profile} alt="Profile icon" />
                   <AiFillCaretDown size={15} />
                   <ul>
@@ -62,156 +63,13 @@ export default function NavBar() {
                     <hr />
                     <li>Sign out of Netflix</li>
                   </ul>
-                </Profile>
+                </Styled.Profile>
               </li>
             </ul>
-          </SecondaryNav>
-        </Container>
-      </Nav>
-      <Spacer></Spacer>
+          </Styled.SecondaryNav>
+        </Styled.Container>
+      </Styled.Nav>
+      <Styled.Spacer></Styled.Spacer>
     </>
   );
 }
-
-const Nav = styled.nav`
-  position: fixed;
-  top: 0;
-  width: 100%;
-  height: 6.8rem;
-  padding: 0 4.6rem;
-  font-size: 1rem;
-  color: white;
-  z-index: 1;
-
-  background-image: linear-gradient(
-    to top,
-    rgba(0, 0, 0, 0),
-    rgba(0, 0, 0, 0.4)
-  );
-
-  ul {
-    display: flex;
-    align-items: center;
-    list-style: none;
-
-    /* @media (min-width: 885px) {
-    show desktop nav, small font
-  } */
-
-    li {
-      margin-left: 2rem;
-
-      a {
-        text-decoration: none;
-        color: white;
-
-        &:hover {
-          color: grey;
-          transition: 800ms;
-        }
-      }
-    }
-  }
-
-  /* @media (min-width: 950px) {
-   expand navbar size
-  } */
-
-  @media (min-width: 1200px) {
-    font-size: 1.4rem;
-  }
-`;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 100%;
-  /* border: 1px solid red; */
-`;
-
-const PrimaryNav = styled.div`
-  display: flex;
-  align-items: center;
-  height: 100%;
-  /* border: 1px solid yellow; */
-
-  img {
-    width: 12rem;
-    margin-right: 1rem;
-  }
-`;
-
-const SecondaryNav = styled.div`
-  display: flex;
-  align-items: center;
-  height: 100%;
-  /* border: 1px solid yellow; */
-
-  img {
-    width: 2.5rem;
-  }
-
-  /* @media (min-width: 400px) {
-    show search function
-  } */
-
-  /* @media (min-width: 1100px) {
-    show 'Kids' link
-  } */
-`;
-
-const Profile = styled.div`
-  display: flex;
-  position: relative;
-  height: 100%;
-
-  img {
-    width: 3rem;
-    border-radius: 5px;
-  }
-
-  ul {
-    display: none;
-    flex-direction: column;
-    align-items: flex-start;
-    position: absolute;
-    top: 50px;
-    left: -150px;
-    width: 20rem;
-    background-color: rgba(0, 0, 0, 0.8);
-    border-radius: 5px;
-
-    li {
-      display: flex;
-      padding: 1rem 0;
-
-      &:nth-of-type(4) {
-        align-self: center;
-        margin-left: 0rem;
-      }
-    }
-
-    hr {
-      width: 100%;
-    }
-  }
-
-  &:hover {
-    ul {
-      display: flex;
-    }
-  }
-
-  /* &:hover{
-
-    ul{
-      display: flex;
-      flex-direction: ;
-    }
-  } */
-`;
-
-const Spacer = styled.div`
-  height: 2000px;
-`;
---
+++ TG-Netflix/src/components/navbar/Navbar.styled.jsx
@@ -0,0 +1,137 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 6.8rem;
+  padding: 0 4.6rem;
+  font-size: 1rem;
+  color: white;
+  z-index: 1;
+
+  background-image: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 0.4)
+  );
+
+  ul {
+    display: flex;
+    align-items: center;
+    list-style: none;
+
+    /* @media (min-width: 885px) {
+    show desktop nav, small font
+  } */
+
+    li {
+      margin-left: 2rem;
+
+      a {
+        text-decoration: none;
+        color: white;
+
+        &:hover {
+          color: grey;
+          transition: 800ms;
+        }
+      }
+    }
+  }
+
+  /* @media (min-width: 950px) {
+   expand navbar size
+  } */
+
+  @media (min-width: 1200px) {
+    font-size: 1.4rem;
+  }
+`;
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  /* border: 1px solid red; */
+`;
+export const PrimaryNav = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  /* border: 1px solid yellow; */
+
+  img {
+    width: 12rem;
+    margin-right: 1rem;
+  }
+`;
+export const SecondaryNav = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  /* border: 1px solid yellow; */
+
+
+
+  /* @media (min-width: 400px) {
+    show search function
+  } */
+
+  /* @media (min-width: 1100px) {
+    show 'Kids' link
+  } */
+`;
+export const Profile = styled.div`
+  display: flex;
+  position: relative;
+  height: 100%;
+
+  img {
+    width: 3rem;
+    border-radius: 5px;
+  }
+
+  ul {
+    display: none;
+    flex-direction: column;
+    align-items: flex-start;
+    position: absolute;
+    top: 50px;
+    left: -150px;
+    width: 20rem;
+    background-color: rgba(0, 0, 0, 0.8);
+    border-radius: 5px;
+
+    li {
+      display: flex;
+      padding: 1rem 0;
+
+      &:nth-of-type(4) {
+        align-self: center;
+        margin-left: 0rem;
+      }
+    }
+
+    hr {
+      width: 100%;
+    }
+  }
+
+  &:hover {
+    ul {
+      display: flex;
+    }
+  }
+
+  /* &:hover{
+
+    ul{
+      display: flex;
+      flex-direction: ;
+    }
+  } */
+`;
+export const Spacer = styled.div`
+  height: 2000px;
+`;
