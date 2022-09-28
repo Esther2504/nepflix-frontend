048bfff99f8e2b22c43ee15575183f2ac8861d69
Esther Schuitemaker <esther252000@gmail.com>
2022-09-28 17:35:07 +0200

    meeste styling, begin collapse divs

--- TG-Netflix/src/pages/account/AccountPage.jsx
+++ TG-Netflix/src/pages/account/AccountPage.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Wrapper } from "./AccountPage.styled";
+import data from "./account.json";
+import { BsChevronDown } from "react-icons/bs";
+import { BsChevronUp } from "react-icons/bs";
 
 export default function AccountPage() {
+const [collapse, setCollapse] = useState(false)
+
+function handleClick(param) {
+  console.log(param)
+  if (collapse != param) {
+  setCollapse(param)
+  } else {
+    setCollapse(false)
+  }
+}
+
+console.log(collapse)
+
   return (
     <Wrapper>
       <nav>Navigatie placeholder</nav>
       <div className="account-container">
         <div className="heading">
-        <h1>Account</h1>
-        <img src="https://assets.nflxext.com/ffe/siteui/account/svg/membersince.svg"></img>
-        <p>Member since September 2022</p>
+          <h1>Account</h1>
+          <img src="https://assets.nflxext.com/ffe/siteui/account/svg/membersince.svg"></img>
+          <p>
+            Member since {data.register_month} {data.register_year}
+          </p>
         </div>
         <div className="account-section membership">
           <div className="left-side">
@@ -19,7 +37,7 @@ export default function AccountPage() {
           <div className="subsections-container">
             <div className="account-subsection">
               <div>
-                <p>johndoe@gmail.com</p>
+                <p>{data.email}</p>
                 <p>Password: *******</p>
               </div>
 
@@ -40,7 +58,10 @@ export default function AccountPage() {
             <hr className="line" />
             <div className="account-subsection">
               <div>
-                <p>Random Bank</p>
+                <div className="payment-method">
+                  <img src={data.payment_logo}></img>{" "}
+                  <p>{data.payment_method}</p>
+                </div>
                 <p>Your next billing date is 13 October 2022.</p>
               </div>
 
@@ -79,13 +100,13 @@ export default function AccountPage() {
           </div>
         </div>
         <div className="account-section plan">
-          <div>
+          <div className="left-side">
             <h2>PLAN DETAILS</h2>
           </div>
           <div className="subsections-container">
             <div className="account-subsection">
               <div>
-                <p>Basic</p>
+                <p>{data.plan}</p>
               </div>
 
               <div className="links">
@@ -98,32 +119,129 @@ export default function AccountPage() {
             </div>
           </div>
         </div>
-        <div className="account-section profile">
-          <div>
+        <div className="account-section">
+          <div className="left-side">
             <h2>PROFILE & PARENTAL CONTROLS</h2>
           </div>
-          {/* Map accounts? */}
-        </div>
-        <div className="account-section settings">
-          <h2>SETTINGS</h2>
-        
-        <div className="account-subsection settings">
-        <div className="links">
-                <ul>
-                  <li>
-                    <a href="">Change email</a>
-                  </li>
-                  <li>
-                    <a href="">Change password</a>
-                  </li>
-                  <li>
-                    <a href="">Add phone number</a>
-                  </li>
-                </ul>
-              </div>
-              </div>
+          <div className="subsections-container">
+            {data.profiles.map((profile) => (
+              <>
+                <div className="profile-section" onClick={() => handleClick(`${profile.id}`)}>
+                  <div className="left">
+                    <img src={profile.avatar} />
+                    <div className="name-rating">
+                      <p>{profile.name}</p>
+                      <p>{profile.rating}</p>
+                    </div>
+                  </div>
+                  <BsChevronUp />
+                </div>
+                <hr className="big-line" />
+                {collapse === `${profile.id}` ? 
+                <>
+                <div className="profile-subsection">
+                  <div>
+                  <p>Language</p>
+                  <p>{profile.language}</p>
+                  </div>
+                  <a href="">Change</a>
+                  </div>
+                  <hr className="small-line" />
+                <div className="profile-subsection">
+                  <div>
+                  <p>Viewing Restrictions</p>
+                  <p>{profile.restrictions}</p>
+                  </div>
+                  <a href="">Change</a>
+                  
+                </div>
+                <hr className="small-line" />
+                <div className="profile-subsection">
+                  <div>
+                  <p>Profile Lock</p>
+                  <p>{profile.profile_lock}</p>
+                  </div>
+                  <a href="">Change</a>
+                  
+                </div>
+                <hr className="small-line" />
+                <div className="profile-subsection">
+                
+                  <p>Viewing activity</p>
+                  <a href="">View</a>
+                 
+                 
+                </div>
+                <hr className="small-line" />
+                <div className="profile-subsection">
+                  
+                  <p>Ratings</p>
+                  <a href="">View</a>
+                  
               
-      </div>
+                </div>
+                <hr className="small-line" />
+                <div className="profile-subsection">
+                
+                  <p>Subtitle appearance</p>
+                  <a href="">Change</a>
+                 
+                 
+                </div>
+                <hr className="small-line" />
+                <div className="profile-subsection">
+                  <div>
+                  <p>Playback settings</p>
+                  <p>{profile.playback_settings}</p>
+                  </div>
+                  <a href="">Change</a>
+                 
+                </div>
+                <hr className="small-line" />
+                <div className="profile-subsection">
+                  <p>Communication settings</p>
+                  <a href="">Change</a>
+                 
+                </div>
+                <hr className="small-line" />
+                <div className="profile-subsection">
+                  <p>Marketing communications</p>
+                  <a href="">Change</a>
+                </div>
+                
+                <hr className="big-line" />
+                </>
+             :
+                null
+}
+              </>
+            ))}
+          </div>
+        </div>
+        <div className="account-section">
+          <h2>SETTINGS</h2>
+          <div className="account-subsection settings">
+            <div className="links">
+              <ul>
+                <li>
+                  <a href="">Test participation</a>
+                </li>
+                <li>
+                  <a href="">Manage download devices</a>
+                </li>
+                <li>
+                  <a href="">Recent device streaming activity</a>
+                </li>
+                <li>
+                  <a href="">Sign out of all devices</a>
+                </li>
+                <li>
+                  <a href="">Download your personal information</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
       {/* <footer>Footer placeholder</footer> */}
     </Wrapper>
--- TG-Netflix/src/pages/account/AccountPage.styled.jsx
+++ TG-Netflix/src/pages/account/AccountPage.styled.jsx
@@ -6,8 +6,20 @@ color: #333333;
 width: 100%
 height: 100vh;
 
+a {
+  text-decoration: none;
+  color: #0073e6;
+  font-weight: 500;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+
 .left-side {
   padding-top: 1.2rem;
+  width: 23%;
 }
 
 .heading {
@@ -55,8 +67,21 @@ height: 100vh;
     border-top: 1px solid #999;
     display: flex;
     justify-content: space-between;
-    font-size: 1.6rem;
+    font-size: 1.54rem;
     padding-bottom: 1.2rem;
+
+    .payment-method {
+      display: flex;
+      font-weight: bold;
+      width: 8rem;
+      justify-content: space-between;
+      
+     
+      img {
+        width: 2.2rem;
+        height: 2.5rem;
+      }
+    }
 
     .line {
       opacity: 0.2;
@@ -78,6 +103,11 @@ height: 100vh;
     }
   }
 
+  .small-line {
+    width: 90%;
+    align-self: flex-end;
+  }
+
   .account-subsection {
     display: flex;
     justify-content: space-between;
@@ -93,24 +123,78 @@ height: 100vh;
         margin: 0 0 6px 0;
       }
 
-      a {
-        text-decoration: none;
-        color: #0073e6;
-        font-weight: 500;
-      }
       }
     }
 
-    .settings {
-      justify-content: flex-start;
-    }
+  }
+
+  .settings {
+    width: 74%;
+    align-self: flex-start;
+  }
+
+.settings li {
+      text-align: left;
   }
 
   .subsections-container {
     display: flex;
     flex-direction: column;
     width: 74%;
+
   }
+
+  .profile-section {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin: 2rem 0 2rem;
+    align-items: center;
+
+    .left {
+      display: flex;
+    }
+
+    img {
+      width: 6rem;
+      height: 6rem;
+      border-radius: 0.5rem;
+    }
+
+    .name-rating {
+      align-self: center;
+
+      p {
+        margin: 0 0 0 1.8rem;
+      }
+
+      p:nth-child(2) {
+        font-size: 1.3rem;
+      }
+    }
+
+ 
+  }
+
+  .profile-subsection {
+    width: 90%;
+    align-self: flex-end;
+    display: flex;
+    justify-content: space-between;
+    margin: 2rem 0 2rem;
+    align-items: center;
+    font-size: 1.3rem;
+
+    p:first-child {
+      font-size: 1.45rem;
+      font-weight: 500;
+      margin-bottom: 0;
+    }
+  }
+
+.big-line {
+  width: 100%;
+}
 
   footer {
     width: 100%;
