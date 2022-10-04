3e9492dfa7b9f7c220be970ede1e5100c2848a9f
Lex <alext@disroot.org>
2022-10-04 23:20:32 +0200

    Add show logo function

--- TG-Netflix/src/components/movie-card/MovieCard.jsx
+++ TG-Netflix/src/components/movie-card/MovieCard.jsx
@@ -5,12 +5,17 @@ import logo from '../../assets/n_logo.ico';
 export default function MovieCard() {
   const title = 'Spider-Man';
 
+  const logoArray = [logo, null];
+  function showLogo(logoArray) {
+    return logoArray[Math.floor(Math.random() * logoArray.length)];
+  }
+
   return (
     <>
       <Card>
         <img src={image} alt={title} />
         <span>{title}</span>
-        <img src={logo} />
+        <img src={showLogo(logoArray)} />
       </Card>
     </>
   );
