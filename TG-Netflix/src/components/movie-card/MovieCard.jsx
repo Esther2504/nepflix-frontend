f2aab91d2f4e4902c9a8609b89411688342d5eb1
Lex <alext@disroot.org>
2022-10-04 09:19:56 +0200

    Inital working version

--- TG-Netflix/src/components/movie-card/MovieCard.jsx
+++ TG-Netflix/src/components/movie-card/MovieCard.jsx
@@ -1,11 +1,37 @@
-import * as S from './MovieCard.styled';
+import { Carousel, Card } from './MovieCard.styled';
+import image from '../../assets/spider-man.jpg';
 
 export default function MovieCard() {
+  const title = 'SPIDER-MAN';
+
   return (
     <>
-      <S.Container>     
-        <h1>SPIDER-MAN</h1>
-      </S.Container>
+      <Carousel>
+        <Card>
+          <img src={image} alt={title} />
+          <span>{title}</span>
+        </Card>
+        <Card>
+          <img src={image} alt={title} />
+          <span>{title}</span>
+        </Card>
+        <Card>
+          <img src={image} alt={title} />
+          <span>{title}</span>
+        </Card>
+        <Card>
+          <img src={image} alt={title} />
+          <span>{title}</span>
+        </Card>
+        <Card>
+          <img src={image} alt={title} />
+          <span>{title}</span>
+        </Card>
+        <Card>
+          <img src={image} alt={title} />
+          <span>{title}</span>
+        </Card>
+      </Carousel>
     </>
   );
 }
--- TG-Netflix/src/components/movie-card/MovieCard.styled.jsx
+++ TG-Netflix/src/components/movie-card/MovieCard.styled.jsx
@@ -1,23 +1,33 @@
 import styled from 'styled-components';
-import image from '../../assets/spider-man.jpg';
 
-export const Container = styled.div`
+export const Carousel = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  width: 100v;
+  padding-left: 1rem;
+`;
+
+export const Card = styled.div`
+  position: relative;
   display: flex;
   flex-wrap: wrap;
-  justify-content: flex-end;
-  align-items: flex-end;
-  width: 34.1rem;
-  height: 19.2rem;
-  padding: .3rem;
+  margin: 0 0.3rem;
+  padding: 0.3rem;
+  text-align: center;
   font-size: 1.6rem;
   font-family: Futura;
   line-height: 0.9;
-  background-color: grey;
-  background-image: url(${image});
+  overflow: hidden;
+  border-radius: 15px;
 
-  h1 {
-    color: transparent;
-    -webkit-text-stroke: 1px white;
-    text-shadow: 1px 1px rgba(0, 0, 0, 0.6);
+  span {
+    position: absolute;
+    bottom: 5px;
+    right: 10px;
+    font-size: 3rem;
+    font-weight: bold;
+    color: white;
+    -webkit-text-stroke: 1px black;
+    text-shadow: none;
   }
 `;
