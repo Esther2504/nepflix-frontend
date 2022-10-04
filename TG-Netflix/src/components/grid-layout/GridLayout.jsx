d0e287191f737a0c3ca6a65632e5eba0c9aa9be6
Esther Schuitemaker <esther252000@gmail.com>
2022-10-04 13:01:36 +0200

    volledige grid layout

--- TG-Netflix/src/components/grid-layout/GridLayout.jsx
+++ TG-Netflix/src/components/grid-layout/GridLayout.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { GridContainer, CardContainer, Card } from './GridLayout.styled'
+import { GridContainer, Card } from './GridLayout.styled'
 
 export default function GridLayout() {
   return (
     <GridContainer>
-      <CardContainer>
         <Card>
           <img src="https://blog.playstation.com/tachyon/2022/06/0c3c20a8d8514501524a0859461f391572ea6e61.jpg?resize=1088%2C612&crop_strategy=smart" />
           <span>Spiderman</span>
@@ -41,8 +40,22 @@ export default function GridLayout() {
           <img src="https://blog.playstation.com/tachyon/2022/06/0c3c20a8d8514501524a0859461f391572ea6e61.jpg?resize=1088%2C612&crop_strategy=smart" />
           <span>Spiderman</span>
         </Card>
-       
-        </CardContainer>
+        <Card>
+          <img src="https://blog.playstation.com/tachyon/2022/06/0c3c20a8d8514501524a0859461f391572ea6e61.jpg?resize=1088%2C612&crop_strategy=smart" />
+          <span>Spiderman</span>
+        </Card>
+        <Card>
+          <img src="https://blog.playstation.com/tachyon/2022/06/0c3c20a8d8514501524a0859461f391572ea6e61.jpg?resize=1088%2C612&crop_strategy=smart" />
+          <span>Spiderman</span>
+        </Card>
+        <Card>
+          <img src="https://blog.playstation.com/tachyon/2022/06/0c3c20a8d8514501524a0859461f391572ea6e61.jpg?resize=1088%2C612&crop_strategy=smart" />
+          <span>Spiderman</span>
+        </Card>
+        <Card>
+          <img src="https://blog.playstation.com/tachyon/2022/06/0c3c20a8d8514501524a0859461f391572ea6e61.jpg?resize=1088%2C612&crop_strategy=smart" />
+          <span>Spiderman</span>
+        </Card>
     </GridContainer>
   )
 }
--- TG-Netflix/src/components/grid-layout/GridLayout.styled.jsx
+++ TG-Netflix/src/components/grid-layout/GridLayout.styled.jsx
@@ -2,46 +2,60 @@ import styled from 'styled-components';
 
 export const GridContainer = styled.div`
 display: flex;
-border: 1px solid blue;
-height: 100%;
-width: 90%;
-margin: auto;
-jusitfy-items: center;
-
-`
-
-export const CardContainer = styled.div`
-display: flex;
-flex-wrap: wrap;
 justify-content: flex-start;
+flex-wrap: wrap;
+height: 100%;
+width: 98%;
+margin: auto;
+padding-left: 4%;
 
 div {
-    margin-bottom: 5rem;
-    flex-basis: 19%;
- }
+  flex-basis: 19%;
+  margin: 0 0 4% 0;
+}
+
+@media screen and (max-width: 1100px) {
+  padding-left: 7%;
+  div {
+    flex-basis: 23%;
+  }
+}
+
+@media screen and (max-width: 800px) {
+  div {
+    margin-bottom: 6%;
+    flex-basis: 30%;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  div {
+    flex-basis: 45%;
+  }
+}
 `
 
 export const Card = styled.div`
-  position: relative;
-  display: flex;
-  flex-wrap: wrap;
-  margin: 0 0.3rem;
-  padding: 0.3rem;
-  text-align: center;
-  font-size: 1.6rem;
-  font-family: Futura;
-  line-height: 0.9;
-  overflow: hidden;
-  background-color: green;
+position: relative;
+display: flex;
+flex-wrap: wrap;
+margin: 0 0.3rem;
+padding: 0.3rem;
+text-align: center;
+font-size: 1.6rem;
+font-family: Futura;
+line-height: 0.9;
+overflow: hidden;
+border-radius: 15px;
 
-  span {
-    position: absolute;
-    bottom: 5px;
-    right: 10px;
-    font-size: 3rem;
-    font-weight: bold;
-    color: white;
-    -webkit-text-stroke: 1px black;
-    text-shadow: none;
-  }
+span {
+  position: absolute;
+  bottom: 5px;
+  right: 10px;
+  font-size: 3rem;
+  font-weight: bold;
+  color: white;
+  -webkit-text-stroke: 1px black;
+  text-shadow: none;
+}
 `;
