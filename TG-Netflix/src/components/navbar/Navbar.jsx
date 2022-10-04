1b195a8a2f149ff1e9a7067aaf70a428a70c3aa3
Lex <alext@disroot.org>
2022-10-04 09:38:08 +0200

    Removed white spaces

--- TG-Netflix/src/components/navbar/Navbar.jsx
+++ TG-Netflix/src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 import { IoMdNotifications } from 'react-icons/io';
 import { RiPencilLine, RiAccountCircleLine } from 'react-icons/ri';
-import { BiHelpCircle} from 'react-icons/bi';
+import { BiHelpCircle } from 'react-icons/bi';
 
 import Searchbar from './Searchbar';
 import * as S from './Navbar.styled';
@@ -25,79 +25,77 @@ export default function Navbar() {
   return (
     <>
       <S.Nav isScrolled={isScrolled}>
-       
-          <S.PrimaryNav>
-            <NavLink to="browse">
-              {' '}
-              <img src={logo} alt="Netflix logo" />
-            </NavLink>
-            <S.Browse>
-              Browse
-              <img src={arrow} />
-            </S.Browse>
-            <ul>
-              <img src={arrow} />
-              <li>
-                <NavLink
-                  to="browse"
-                  style={({ isActive }) => (isActive ? activeStyle : undefined)}
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="films"
-                  style={({ isActive }) => (isActive ? activeStyle : undefined)}
-                >
-                  Films
-                </NavLink>
-              </li>
-            </ul>
-          </S.PrimaryNav>
-          <S.SecondaryNav>
-            <ul>
-              <li>
-                <Searchbar />
-              </li>
-              <li>
-                <S.Notifications>
-                  <IoMdNotifications size={30} />
-                  <ul>
-                    <img src={arrow} />
-                    <li>No recent notifications</li>
-                  </ul>
-                </S.Notifications>
-              </li>
-              <li>
-                {' '}
-                <S.Account>
-                  <img src={profile} alt="Profile icon" />
+        <S.PrimaryNav>
+          <NavLink to="browse">
+            {' '}
+            <img src={logo} alt="Netflix logo" />
+          </NavLink>
+          <S.Browse>
+            Browse
+            <img src={arrow} />
+          </S.Browse>
+          <ul>
+            <img src={arrow} />
+            <li>
+              <NavLink
+                to="browse"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="films"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                Films
+              </NavLink>
+            </li>
+          </ul>
+        </S.PrimaryNav>
+        <S.SecondaryNav>
+          <ul>
+            <li>
+              <Searchbar />
+            </li>
+            <li>
+              <S.Notifications>
+                <IoMdNotifications size={30} />
+                <ul>
                   <img src={arrow} />
-                  <ul>
-                    <img src={arrow} />
-                    <li>
-                      <RiPencilLine size={20} />
-                      <a href="#">Manage Profiles</a>
-                    </li>
-                    <li>
-                      <RiAccountCircleLine size={20} />
-                      <a href="/account">Account</a>
-                    </li>
-                    <li>
-                      <BiHelpCircle size={20} />
-                      <a href="#">Help Centre</a>
-                    </li>
-                    <hr />
-                    <li>
-                      <a href="#">Sign out of Netflix</a>
-                    </li>
-                  </ul>
-                </S.Account>
-              </li>
-            </ul>
-          </S.SecondaryNav>
-      
+                  <li>No recent notifications</li>
+                </ul>
+              </S.Notifications>
+            </li>
+            <li>
+              {' '}
+              <S.Account>
+                <img src={profile} alt="Profile icon" />
+                <img src={arrow} />
+                <ul>
+                  <img src={arrow} />
+                  <li>
+                    <RiPencilLine size={20} />
+                    <a href="#">Manage Profiles</a>
+                  </li>
+                  <li>
+                    <RiAccountCircleLine size={20} />
+                    <a href="/account">Account</a>
+                  </li>
+                  <li>
+                    <BiHelpCircle size={20} />
+                    <a href="#">Help Centre</a>
+                  </li>
+                  <hr />
+                  <li>
+                    <a href="#">Sign out of Netflix</a>
+                  </li>
+                </ul>
+              </S.Account>
+            </li>
+          </ul>
+        </S.SecondaryNav>
       </S.Nav>
       <S.VerticalSpacer />
     </>
