ab0a05955179a881475bd3640fb3f649ec0511dc
Luisa Wolter <93677938+LuiWo@users.noreply.github.com>
2022-10-07 09:36:12 +0200

    route change for Landing Page

--- TG-Netflix/src/pages/index.js
+++ TG-Netflix/src/pages/index.js
@@ -1,4 +1,4 @@
-export { default as Landing } from './landing/Landing';
+export { default as Landing } from '../components/landing-page/SignIn';
 export { default as Discover } from './discover/Discover';
 export { default as Films } from './films/Films';
 export { default as Account } from './account/AccountPage';
