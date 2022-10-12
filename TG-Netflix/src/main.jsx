d4fa0daba8c59591ae37e2e8d020883e374fdeba
Nathaniel Brunk <Nathaniel_Brunk@hotmail.com>
2022-10-12 10:39:56 +0200

    Update main
    Added <Provider> if  we might need it later.

--- TG-Netflix/src/main.jsx
+++ TG-Netflix/src/main.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
 import App from "./App";
 
+import reducers from "./reducers";
+const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
