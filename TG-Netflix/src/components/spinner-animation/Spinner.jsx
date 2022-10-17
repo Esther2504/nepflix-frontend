686ee2e5a6931e248f397126450fef178857cea5
Bart Erkamp <b.erkamp@gmail.com>
2022-10-17 11:14:00 +0200

    add spinner component

---
+++ TG-Netflix/src/components/spinner-animation/Spinner.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const Spinner = () => {
+  return <Spinning></Spinning>;
+};
+
+export default Spinner;
+
+const rotate = keyframes`
+ 0% {
+        transform: translate3d(-50%, -50%, 0) rotate(0deg);
+    }
+    100% {
+         transform: translate3d(-50%, -50%, 0) rotate(360deg);
+    }
+`;
+
+const Spinning = styled.div`
+  position: relative;
+  opacity: 1;
+  transition: opacity linear 0.1s; 
+  /* border: 1px solid black; */
+  height: 5rem;
+
+  &::before {
+    animation: ${rotate} 2s linear infinite;
+    border: solid 3px #eee;
+    border-bottom-color: #db0000;
+    border-radius: 50%;
+    content: "";
+    height: 40px;
+    left: 50%;
+    top: 50%; 
+    opacity: inherit;
+    position: absolute;
+    transform: translate3d(-50%, -50%, 0);
+    transform-origin: center;
+    width: 40px;
+    will-change: transform;
+  }
+`;
