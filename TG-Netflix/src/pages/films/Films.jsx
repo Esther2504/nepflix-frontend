2f2979a2db5a5a28f71077697ac84dd9d540065e
Bart Erkamp <b.erkamp@gmail.com>
2022-10-17 11:42:41 +0200

    place styles in seperate file, add spinner component to films page for review

--- TG-Netflix/src/pages/films/Films.jsx
+++ TG-Netflix/src/pages/films/Films.jsx
@@ -1,9 +1,11 @@
 import Footer from '../../components/footer/footer';
+import Spinner from '../../components/spinner-animation/Spinner';
 
 export default function Films() {
   return (
     <>
       <div className="members-container">
+        <Spinner/>
         <Footer />
       </div>
     </>
