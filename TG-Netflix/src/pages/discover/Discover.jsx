5b77f797675b90bc75887015bb53a17ac7db5134
Szymon M <szymon.mrozek@hotmail.com>
2022-10-24 16:00:17 +0200

    spread data over discovery page banner

--- TG-Netflix/src/pages/discover/Discover.jsx
+++ TG-Netflix/src/pages/discover/Discover.jsx
@@ -1,13 +1,14 @@
 import Player from "../../components/player/Player";
 import Footer from "../../components/footer/Footer";
 
-export default function Discover() {
-
+// props kunnen worden doorgegeven worden vanaf main om content te laden voordat
+// bezoeker inlogt
+export default function Discover({banner, categories}) {
     return (
         <>
             <div className="members-container">
                 {/* CONTENT GOES HERE */}
-                <Player />
+                <Player data={banner}/>
                 <Footer />
             </div>
         </>
