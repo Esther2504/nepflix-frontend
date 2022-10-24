1b41da3c9916ab2653384ce844ee56728721da06
Sander Grunnekemeijer <sander@grunnekemeijer.net>
2022-10-24 11:43:58 +0200

    fix error

--- TG-Netflix/src/components/Modal/test/CallModal.jsx
+++ TG-Netflix/src/components/Modal/test/CallModal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "React";
+import React, { useRef, useEffect, useState } from "react";
 import { Child } from "../test/CallModal.styled";
 
 import CallSmallModal from "./CallSmallModal";
