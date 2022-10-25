3b6372da4dc1ddf7ab243a3c7b3a53cb6f05bca0
Luisa Wolter <93677938+LuiWo@users.noreply.github.com>
2022-10-25 12:19:34 +0200

    fetched data in global state + local storage persist

--- TG-Netflix/src/App.jsx
+++ TG-Netflix/src/App.jsx
@@ -1,4 +1,7 @@
-
+// calls fetched data
+import { useDispatch } from "react-redux";
+import { getMovies, getBrowse } from "./reducers/fetchReducer";
+import { useEffect } from "react";
 
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
@@ -14,13 +17,18 @@ function App() {
     "/grid-layout",
   ];
 
+  //Calls data from GlobalState
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMovies());
+    dispatch(getBrowse());
+  }, []);
+
   return (
     <>
       {mainUIRoutes.includes(location.pathname) && <Navbar />}
 
       <Outlet />
-      
-      
     </>
   );
 }
