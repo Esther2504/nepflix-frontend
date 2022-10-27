0f072b6acf03c2b4b7a2351c4f81cdc1433ecb4a
Lex <alext@disroot.org>
2022-10-27 16:21:28 +0200

    Add player, lanes to home and films pages

--- TG-Netflix/src/main.jsx
+++ TG-Netflix/src/main.jsx
@@ -30,7 +30,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             <Route path="/" element={<App />}>
               <Route index element={<Landing />} />
               <Route path="browse" element={<Discover banner={browseMockData.banner} categories={browseMockData.categories} movie={movieDetailsMock}/>} />
-              <Route path="films" element={<Films />} />
+              <Route path="films" element={<Films banner={browseMockData.banner} categories={browseMockData.categories} movie={movieDetailsMock} />} />
               <Route path="my-list" element={<MyList />} />
               <Route path="account" element={<Account />} />
               <Route path="lanes" element={<Lanes categories={browseMockData.categories} movie={movieDetailsMock} />} />
