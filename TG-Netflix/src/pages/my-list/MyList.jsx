0e712f220b3e4b6e9f7e6bc94ec95827926312cc
Sander Grunnekemeijer <sander@grunnekemeijer.net>
2022-10-31 14:47:46 +0100

    My List page basic styling

--- TG-Netflix/src/pages/my-list/MyList.jsx
+++ TG-Netflix/src/pages/my-list/MyList.jsx
@@ -1,10 +1,15 @@
 import Footer from '../../components/footer/footer';
-
+import GridLayout from '../../components/grid-layout/GridLayout';
+import categoriesMock from '../../mock-data/browse_categories_banner.mock.json';
+import {Title} from './MyList.styled'
 export default function MyList() {
   return (
     <>
       <div className="padding-container">
-        {/* CONTENT GOES HERE */}
+
+        
+        <Title>My List</Title>
+        <GridLayout movies={categoriesMock.categories[0].movies} />
         <Footer />
       </div>
     </>
---
+++ TG-Netflix/src/pages/my-list/MyList.styled.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+
+color:white;
+padding-left: 3ren;
+`;
