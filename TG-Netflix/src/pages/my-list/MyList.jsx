e47be79726712234538a5a8430d4b290b8eeab35
Bart Erkamp <b.erkamp@gmail.com>
2022-11-01 09:54:08 +0100

    new spinner animation with dark background

--- TG-Netflix/src/pages/my-list/MyList.jsx
+++ TG-Netflix/src/pages/my-list/MyList.jsx
@@ -1,10 +1,15 @@
 import Footer from '../../components/footer/footer';
+// onderstaande import verwijderen na review
+import Spinner from '../../components/spinner-animation/Spinner';
 
 export default function MyList() {
   return (
     <>
       <div className="padding-container">
         {/* CONTENT GOES HERE */}
+
+        {/* spinner component verwijderen na review */}
+        <Spinner/>
         <Footer />
       </div>
     </>
