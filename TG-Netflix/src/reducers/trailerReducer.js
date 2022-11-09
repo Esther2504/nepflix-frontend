21b7f68a42d4f2bcd920fd8007d9480c3ae92480
Nathaniel Brunk <Nathaniel_Brunk@hotmail.com>
2022-11-09 16:30:04 +0100

    Update trailerReducer.js

--- TG-Netflix/src/reducers/trailerReducer.js
+++ TG-Netflix/src/reducers/trailerReducer.js
@@ -1,8 +1,26 @@
-export default (state = [], action) => {
-    switch (action.type) {
-        case 'FETCH_TRAILER':
-            return action.payload;
-        default:
-            return state;
+import { createSlice } from "@reduxjs/toolkit";
+import YouTube from "react-youtube";
+
+const initialState = {
+    seconds: 0,
+}
+
+export const TrailerSlice = createSlice({
+    name: 'trailer',
+    initialState,
+    reducers: {
+        getCurrentTime: (state) => {
+            YouTubePlayer.getCurrentTime()
+        },
+        setCurrentTime: (state) => {
+            YouTubePlayer.seekTo(
+                seconds,
+                allowSeekAhead = true)
+        }
     }
-};
+}
+)
+
+export const { getCurrentTime, setCurrentTime } = TrailerSlice.actions;
+
+export default TrailerSlice.reducer;
