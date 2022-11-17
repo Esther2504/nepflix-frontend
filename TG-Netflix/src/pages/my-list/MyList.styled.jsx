3e9eea75ab12bfacc074d831660014d2e2400256
Sander Grunnekemeijer <sander@grunnekemeijer.net>
2022-11-17 10:10:19 +0100

    final final final commit

--- TG-Netflix/src/pages/my-list/MyList.styled.jsx
+++ TG-Netflix/src/pages/my-list/MyList.styled.jsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
 display: flex;
 flex-direction: column;
+width: 100%;
 align-items: center;
 justify-content: flex-start;
 `;
