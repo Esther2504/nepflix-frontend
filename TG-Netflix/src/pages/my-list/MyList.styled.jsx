b35e0e2c3a0f4efa1862358147ce4cbbab71337b
Sander Grunnekemeijer <sander@grunnekemeijer.net>
2022-11-17 10:01:32 +0100

    dots on the I

--- TG-Netflix/src/pages/my-list/MyList.styled.jsx
+++ TG-Netflix/src/pages/my-list/MyList.styled.jsx
@@ -1,46 +1,62 @@
+import { BsFileX } from 'react-icons/bs';
 import styled from 'styled-components';
+
+
+export const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: flex-start;
+`;
+
 
 export const Title = styled.h2`
   color: #e5e5e5;
 `;
 
+
 export const TitleWrapper = styled.div`
 display: flex;
 flex-direction: row;
-align-items: center;
-justify-content: flex-start;
-margin: auto;
+width: 100%;
 padding-left: 3%;
 font-size: 2rem;
 flex-basis: 19%;
-margin-left: .5px;
-margin-bottom: 4%;
+margin-bottom: 2rem;
+
+
+div {
+  flex-basis: 19%;
+  margin-left: .5px;
+  margin-bottom: 4%;
+}
 
 @media screen and (max-width: 1100px) {
-  font-size: 2rem;
   padding-left: 6%;
   div {
     flex-basis: 23%;
   }
 }
-@media screen and (max-width: 885px) {
-  font-size: 1.6rem;
+
+@media screen and (max-width: 800px) {
   padding-left: 5%;
   div {
+    margin-bottom: 6%;
     flex-basis: 31%;
   }
 }
+
 @media screen and (max-width: 500px) {
-  font-size: 1.3rem;
   padding-left: 3%;
   div {
     flex-basis: 47%;
   }
 }
 
-  
-
 `;
+
+
+
 
 
 
